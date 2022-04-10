87454cbc8985cc6f834fdc49501f137387152d52
KayCodes11 <kayydraws@gmail.com>
2022-04-10 19:33:44 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import { io } from "socket.io-client";
+import "./App.css";
+
+const socket = io("http://localhost:8000"); // connect to backend socket server
+
+function App() {
+  const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const joinRoom = () => {
+    if (room !== '')  {
+      socket.emit("join_room", room);
+    }
+  };
+
+  const sendMessage = () => {
+    // socket.emit("hello from the client", 5, "6", { 7: Uint8Array.from([8]) });
+    socket.emit("send_message", { message, room });
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <div>
+
+        <input
+            type={"text"}
+            placeholder="Room Number"
+            onChange={(e) => setRoom(e.target.value)}
+          />
+          <button onClick={joinRoom}>Join Room</button>
+          <hr/>
+          <input
+            type={"text"}
+            placeholder="write message"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button onClick={sendMessage}>send message</button>
+          <h1>Message: </h1>
+          {messageReceived}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const rootElement = document.getElementById('root');
+if(!rootElement) throw new Error(`Failed to find the root element`);
+
+// @ts-ignore
+// const root1 = React.createRoot(document.getElementById('root')!); 
+// telling ts compiller its value would never be  null
+
+// @ts-ignore
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
